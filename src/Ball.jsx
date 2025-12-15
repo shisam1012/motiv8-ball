b6001c2713464ac155a18366bbd436c92430882d
shisam1012 <shsa10121999@gmail.com>
2025-12-15 18:54:42 +0200

    attempt to fix SVG rendering issues on iOS

--- src/Ball.jsx
+++ src/Ball.jsx
@@ -4,36 +4,56 @@ import { sentences } from './Sentences';
 
 const Ball = () => {
     const [animationKey, setAnimationKey] = useState(0);
-
     const [currentSentence, setCurrentSentence] = useState(sentences[0]);
 
     const handleBallClick = () => {
         const randomIndex = Math.floor(Math.random() * (sentences.length - 1));
-        const newSentence = sentences[randomIndex + 1];
-        setCurrentSentence(newSentence);
-
+        setCurrentSentence(sentences[randomIndex + 1]);
         setAnimationKey((prevKey) => prevKey + 1);
     };
 
     return (
         <div className='black-circle' onClick={handleBallClick}>
             <div className='white-circle'>
-                <svg viewBox='0 0 100 100'>
-                    <path
-                        id='myArc'
-                        d='M 10 50 A 40 40 0 0 1 90 50'
-                        fill='none'
-                        stroke='none'
-                    />
+                <svg
+                    viewBox='0 0 100 100'
+                    width='100%'
+                    height='100%'
+                    preserveAspectRatio='xMidYMid meet'
+                >
+                    <defs>
+                        <path id='myArc' d='M 10 50 A 40 40 0 0 1 90 50' />
 
-                    <g key={animationKey} className='zoom-in-text text-glow'>
+                        <filter
+                            id='blueGlow'
+                            x='-60%'
+                            y='-60%'
+                            width='220%'
+                            height='220%'
+                        >
+                            <feGaussianBlur stdDeviation='4' result='blur1' />
+                            <feGaussianBlur stdDeviation='2' result='blur2' />
+
+                            <feMerge>
+                                <feMergeNode in='blur1' />
+                                <feMergeNode in='blur2' />
+                                <feMergeNode in='SourceGraphic' />
+                            </feMerge>
+                        </filter>
+                    </defs>
+
+                    <g
+                        key={animationKey}
+                        className='zoom-in-text'
+                        filter='url(#blueGlow)'
+                    >
                         <text
-                            fontFamily='Arial'
+                            fontFamily='Arial, sans-serif'
                             fontSize='6.5'
-                            fill='#235fe0ff'
+                            fill='#235fe0'
                         >
                             <textPath
-                                href='#myArc'
+                                xlinkHref='#myArc'
                                 startOffset='50%'
                                 textAnchor='middle'
                             >
@@ -44,8 +64,8 @@ const Ball = () => {
 
                     <polygon
                         points='30,40 70,40 50,80'
-                        fill='#235fe0ff'
-                        className='triangle-glow'
+                        fill='#235fe0'
+                        filter='url(#blueGlow)'
                     />
                 </svg>
             </div>
